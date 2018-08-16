0d8abb87928213ebc386eb5928c85ff90ac9eacb
bobtal <talevski.boban@gmail.com>
2018-08-16 20:11:31 +0200

    Moves the '3/' part of BASE_URL back in the BASE_URL and removes the leading '/' from the paths in the interface methods, making the url resolve correctly without omitting the version number in the path. Getting 200 instead of 404 again.

--- app/src/main/java/com/gmail/at/boban/talevski/popularmovies/api/MovieDbApi.java
+++ app/src/main/java/com/gmail/at/boban/talevski/popularmovies/api/MovieDbApi.java
@@ -11,9 +11,9 @@ import retrofit2.http.Query;
 
 public interface MovieDbApi {
 
-    @GET("/3/movie/popular")
+    @GET("movie/popular")
     Call<MovieDbResponse> getPopularMovies(@Query("api_key") String apiKey);
 
-    @GET("/3/movie/top_rated")
+    @GET("movie/top_rated")
     Call<MovieDbResponse> getTopRatedMovies(@Query("api_key") String apiKey);
 }
