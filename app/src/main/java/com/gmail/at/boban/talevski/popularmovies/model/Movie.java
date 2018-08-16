1743ef20e1a39302164d640c826631651d3e4078
bobtal <talevski.boban@gmail.com>
2018-08-16 18:39:54 +0200

    Adds constructor, getters and setters in Movie.java

--- app/src/main/java/com/gmail/at/boban/talevski/popularmovies/model/Movie.java
+++ app/src/main/java/com/gmail/at/boban/talevski/popularmovies/model/Movie.java
@@ -1,9 +1,70 @@
 package com.gmail.at.boban.talevski.popularmovies.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Movie {
+    @SerializedName("title")
+    @Expose
     public String title;
-    public String poster_path;
+    @SerializedName("poster_path")
+    @Expose
+    public String posterPath;
+    @SerializedName("overview")
+    @Expose
     public String overview;
-    public double vote_average;
-    public String release_date;
+    @SerializedName("vote_average")
+    @Expose
+    public double voteAverage;
+    @SerializedName("release_date")
+    @Expose
+    public String releaseDate;
+
+    public Movie(String title, String posterPath, String overview, double voteAverage, String releaseDate) {
+        this.title = title;
+        this.posterPath = posterPath;
+        this.overview = overview;
+        this.voteAverage = voteAverage;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
