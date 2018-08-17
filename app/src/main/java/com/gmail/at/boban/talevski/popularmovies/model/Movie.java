d9e69ce4587103b953be288d858ba23f13754e6a
bobtal <talevski.boban@gmail.com>
2018-08-17 19:27:46 +0200

    Adds id field to Movie model class and make it implement Parcelable

--- app/src/main/java/com/gmail/at/boban/talevski/popularmovies/model/Movie.java
+++ app/src/main/java/com/gmail/at/boban/talevski/popularmovies/model/Movie.java
@@ -1,9 +1,12 @@
 package com.gmail.at.boban.talevski.popularmovies.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Movie {
+public class Movie implements Parcelable{
     @SerializedName("title")
     @Expose
     public String title;
@@ -19,13 +22,17 @@ public class Movie {
     @SerializedName("release_date")
     @Expose
     public String releaseDate;
+    @SerializedName("id")
+    @Expose
+    public int id;
 
-    public Movie(String title, String posterPath, String overview, double voteAverage, String releaseDate) {
+    public Movie(String title, String posterPath, String overview, double voteAverage, String releaseDate, int id) {
         this.title = title;
         this.posterPath = posterPath;
         this.overview = overview;
         this.voteAverage = voteAverage;
         this.releaseDate = releaseDate;
+        this.id = id;
     }
 
     public String getTitle() {
@@ -67,4 +74,48 @@ public class Movie {
     public void setReleaseDate(String releaseDate) {
         this.releaseDate = releaseDate;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(posterPath);
+        parcel.writeString(overview);
+        parcel.writeDouble(voteAverage);
+        parcel.writeString(releaseDate);
+        parcel.writeInt(id);
+    }
+
+    protected Movie(Parcel in) {
+        title = in.readString();
+        posterPath = in.readString();
+        overview = in.readString();
+        voteAverage = in.readDouble();
+        releaseDate = in.readString();
+        id = in.readInt();
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
