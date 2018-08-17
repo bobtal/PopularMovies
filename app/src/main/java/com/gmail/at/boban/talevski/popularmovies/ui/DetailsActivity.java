86217cc537cd710737381cad6980a03959e67ab2
bobtal <talevski.boban@gmail.com>
2018-08-17 19:28:29 +0200

    Adds DetailsActivity for showing movie details

---
+++ app/src/main/java/com/gmail/at/boban/talevski/popularmovies/ui/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.gmail.at.boban.talevski.popularmovies.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gmail.at.boban.talevski.popularmovies.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
